133fda6787addd8a9c45cbe686ccbd3a1dd3a77b
Wouter Nivelle <wouternivelle@gmail.com>
2022-12-06 14:16:08 +0100

    Fixed tests with authentication

--- src/test/java/io/nivelle/finansaurus/CommonIntegrationTest.java
+++ src/test/java/io/nivelle/finansaurus/CommonIntegrationTest.java
@@ -7,9 +7,11 @@ import io.nivelle.finansaurus.payees.domain.PayeeRepository;
 import io.nivelle.finansaurus.transactions.domain.Transaction;
 import io.nivelle.finansaurus.transactions.domain.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.support.TransactionTemplate;
 
@@ -17,7 +19,14 @@ import javax.persistence.EntityManager;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = {"classpath:truncate.sql"})
+@TestPropertySource(properties = {
+        "security.username=user",
+        "security.password=password"})
 public abstract class CommonIntegrationTest {
+    @Value("${security.username}")
+    private String username;
+    @Value("${security.password}")
+    private String password;
 
     @LocalServerPort
     protected int port;
@@ -42,8 +51,12 @@ public abstract class CommonIntegrationTest {
     protected BalanceRepository balanceRepository;
 
     protected Transaction saveTransaction(Transaction transaction) {
-        return restTemplate.postForEntity("http://localhost:" + port + "/transactions", transaction, Transaction.class)
+        return getRestTemplate().postForEntity("http://localhost:" + port + "/transactions", transaction, Transaction.class)
                 .getBody();
+    }
+
+    protected TestRestTemplate getRestTemplate() {
+        return restTemplate.withBasicAuth(username, password);
     }
 
     protected void runInTransaction(Runnable runnable) {
--- src/test/java/io/nivelle/finansaurus/FinansaurusApplicationTests.java
+++ src/test/java/io/nivelle/finansaurus/FinansaurusApplicationTests.java
@@ -1,10 +1,8 @@
 package io.nivelle.finansaurus;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FinansaurusApplicationTests {
+class FinansaurusApplicationTests extends CommonIntegrationTest {
 
     @Test
     void contextLoads() {
