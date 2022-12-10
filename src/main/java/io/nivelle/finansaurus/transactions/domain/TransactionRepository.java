2aa61909b250215503d2d8fe37435911ff80c497
Wouter Nivelle <wouternivelle@gmail.com>
2022-12-10 13:13:09 +0100

    Show initial transactions for balance

--- src/main/java/io/nivelle/finansaurus/transactions/domain/TransactionRepository.java
+++ src/main/java/io/nivelle/finansaurus/transactions/domain/TransactionRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends PagingAndSortingRepository<Transaction, Long> {
     @Query("select t from Transaction t where " +
+            "(t.categoryId = (select c.id from Category c where c.type = 'INITIAL') and t.date >= add_months(adddate(last_day(:date), 1), -1) and t.date <= last_day(:date)) OR " +
             "(t.categoryId = (select c.id from Category c where c.type = 'INCOME_CURRENT_MONTH') and t.date >= add_months(adddate(last_day(:date), 1), -1) and t.date <= last_day(:date)) OR " +
             "(t.categoryId = (select c.id from Category c where c.type = 'INCOME_NEXT_MONTH') and t.date >= add_months(adddate(last_day(:date), 1), -2) and t.date <= last_day(add_months(:date, -1)))")
     List<Transaction> listIncomingForBalance(LocalDate date);
