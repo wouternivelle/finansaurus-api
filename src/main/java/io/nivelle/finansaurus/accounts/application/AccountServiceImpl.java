9687ce51228c078cfedb51f9cbce3bf0de172011
Wouter Nivelle <wouternivelle@gmail.com>
2022-12-10 12:20:31 +0100

    Update incoming balance with new account

--- src/main/java/io/nivelle/finansaurus/accounts/application/AccountServiceImpl.java
+++ src/main/java/io/nivelle/finansaurus/accounts/application/AccountServiceImpl.java
@@ -3,6 +3,8 @@ package io.nivelle.finansaurus.accounts.application;
 import io.nivelle.finansaurus.accounts.domain.Account;
 import io.nivelle.finansaurus.accounts.domain.AccountNotFoundException;
 import io.nivelle.finansaurus.accounts.domain.AccountRepository;
+import io.nivelle.finansaurus.balances.domain.Balance;
+import io.nivelle.finansaurus.balances.domain.BalanceRepository;
 import io.nivelle.finansaurus.categories.domain.CategoryRepository;
 import io.nivelle.finansaurus.categories.domain.CategoryType;
 import io.nivelle.finansaurus.transactions.domain.Transaction;
@@ -22,12 +24,14 @@ public class AccountServiceImpl implements AccountService {
     private AccountRepository repository;
     private TransactionRepository transactionRepository;
     private CategoryRepository categoryRepository;
+    private BalanceRepository balanceRepository;
 
     @Autowired
-    public AccountServiceImpl(AccountRepository repository, TransactionRepository transactionRepository, CategoryRepository categoryRepository) {
+    public AccountServiceImpl(AccountRepository repository, TransactionRepository transactionRepository, CategoryRepository categoryRepository, BalanceRepository balanceRepository) {
         this.repository = repository;
         this.transactionRepository = transactionRepository;
         this.categoryRepository = categoryRepository;
+        this.balanceRepository = balanceRepository;
     }
 
     @Override
@@ -40,6 +44,8 @@ public class AccountServiceImpl implements AccountService {
         if (isNew) {
             Transaction transaction = Transaction.builder().accountId(account.getId()).type(TransactionType.IN).amount(account.getAmount()).categoryId(categoryRepository.findCategoryByType(CategoryType.INITIAL).getId()).date(LocalDate.now()).build();
             transactionRepository.save(transaction);
+
+            addToBalance(transaction);
         }
 
         return account;
@@ -62,5 +68,22 @@ public class AccountServiceImpl implements AccountService {
     @Override
     public Page<Account> list(Pageable pageable) {
         return repository.findAll(pageable);
+    }
+
+    private void addToBalance(Transaction transaction) {
+        Balance balance = findOrCreateBalance(transaction);
+
+        balance.updateIncoming(balance.getIncoming().add(transaction.getAmount()));
+        balanceRepository.save(balance);
+    }
+
+    private Balance findOrCreateBalance(Transaction transaction) {
+        LocalDate date = transaction.getDate();
+
+        int month = date.getMonthValue();
+        int year = date.getYear();
+
+        return balanceRepository.findByMonthAndYear(month, year)
+                .orElseGet(() -> balanceRepository.save(Balance.builder().month(month).year(year).build()));
     }
 }
